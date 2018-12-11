697ef88f27a672103e9832303a07167b85a7b747
Alf-Kenneth Aabel <aka@decisive.no>
2018-12-11 20:24:30 +0100

    Svar for oppgave 1 dag 1.

--- day1.clj
+++ day1.clj
@@ -1,4 +1,22 @@
-(ns adventofcode2018)
+(ns adventofcode2018
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+
+(def tallene (slurp "inputday1.txt"))
+
+(def lines (str/split-lines tallene))
 
 (println "hello")
 
+(defn tonum [astr]
+  (Integer/valueOf astr))
+
+(println (reduce + (map tonum lines)))
+
+(comment
+  ;;(def data-file (io/resource "https://adventofcode.com/2018/day/1/input"))
+
+  (defn get-lines []
+    (str/split-lines (slurp file)))
+)
