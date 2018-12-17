69160674d4a2f500328f30152d38c08ab02763ff
Alf-Kenneth Aabel <aka@decisive.no>
2018-12-17 22:37:38 +0100

    Fixet tester til å fungere (tror jeg), men mangler litt mer testing og å lære meg debugger i Emacs.

--- day2.clj
+++ day2.clj
@@ -1,6 +1,7 @@
 (ns adventofcode2018
   (:require [clojure.java.io :as io])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use clojure.test))
 
 
 (defn tonum [astr]
@@ -12,33 +13,56 @@
 
 (def intlist (map tonum lines))
 
-(defn findfreq [sum numlist dict]
-  (if (empty? numlist)
-    [sum [] dict]
-    (let [[firstnum & the-rest] numlist
-          newsum (+ firstnum sum)
-          numfreq (get dict newsum 0)]
-      (if (= numfreq 2)
-        [newsum nil nil]
-        (findfreq newsum the-rest (assoc dict newsum (+ numfreq 1)))))))
 
 
-(defn findall [alist]
+
+(defn findfreq [alist]
   (loop [currsum 0
          thelist alist
          dict {}]
-    (let [[sum somelist somedict] (findfreq currsum alist dict) ]
-      (if (nil? somelist)
-        (println (str "Found solution: " sum))
-        (recur sum somelist somedict)))))
+    (let [[firstnum & rest] thelist
+          found? (not= 0 (get dict currsum 0))]
+      (if found?
+        currsum
+        (recur (+ currsum firstnum) rest (assoc dict currsum 1))))))
+
+(deftest test-one-minus-one
+  (is (= 0 (findfreq (cycle [1 -1])))))
+
+(deftest test-2
+  (is (= 10 (findfreq [3 3 4 -2 -4]))))
+
 
 
 
 (comment
+  ;; Eksempel på test
+  (deftest entest
+    (is (= "ABC" (str/capitalize "abc"))))
+
   ;;(def data-file (io/resource "https://adventofcode.com/2018/day/1/input"))
 
   ;;(println (reduce + (map tonum lines)))
 
   (defn get-lines []
     (str/split-lines (slurp file)))
-)
+
+  ;; Old try
+  (defn findfreq [sum numlist dict]
+    (if (empty? numlist)
+      [sum [] dict]
+      (let [[firstnum & the-rest] numlist
+            newsum (+ firstnum sum)
+            numfreq (get dict newsum 0)]
+        (if (= numfreq 2)
+          [newsum nil nil]
+          (findfreq newsum the-rest (assoc dict newsum (+ numfreq 1)))))))
+
+  (defn findfreq [alist]
+    (loop [currsum 0
+           thelist alist
+           dict {}]
+      (let [[sum somelist somedict] (findfreq currsum alist dict) ]
+        (if (nil? somelist)
+          (println (str "Found solution: " sum))
+          (recur sum somelist somedict)))))  )
